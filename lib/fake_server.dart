d2111e25387d6627c695e6aae8b61fbe7db5cbf4
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-08-04 21:40:39 +0300

    added server variability

--- lib/fake_server.dart
+++ lib/fake_server.dart
@@ -7,10 +7,13 @@ class FakeServer {
     await Future.delayed(Duration(seconds: 3));
 
     if ((requestNumber + 1) % 3 == 0) {
+      requestNumber += 1;
       return '{"lon":-0.13,"lat":51.51}';
     } else if ((requestNumber + 1) % 3 == 1) {
+      requestNumber += 1;
       return '{"msg":"Auth error"}';
     } else if ((requestNumber + 1) % 3 == 2) {
+      requestNumber += 1;
       return '<html> <some>';
     }
   }
