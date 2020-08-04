08eacf91b5faa575eaf6e63b955eca4aca5d412e
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-08-04 23:03:34 +0300

    corrected the server

--- lib/fake_server.dart
+++ lib/fake_server.dart
@@ -1,34 +1,18 @@
 import 'dart:async';
 
-import 'package:rxdart/rxdart.dart';
-
 class FakeServer {
   int requestNumber = 0;
 
-  PublishSubject<String> subject = new PublishSubject();
-
-  Stream<String> getWeather() {
-    new Timer(Duration(seconds: 2), () {
-      int state = (requestNumber ) % 3;
-      print("Sending Response" + state.toString());
-      if (state == 0) {
-        requestNumber++;
-        subject.add('{"lon":-0.13,"lat":51.51}');
-      }
-      if (state == 1) {
-        requestNumber++;
-        subject.add('{"msg":"Auth error"}');
-      }
-      if (state == 2) {
-        requestNumber++;
-        subject.add('<html> <some>');
-      }
-    });
-
-    return subject.stream;
-  }
-
-  dispose() {
-    subject.close();
+  Future<String> getWeather() async {
+    await Future.delayed(Duration(seconds: 5));
+    int state = (requestNumber) % 3;
+    requestNumber++;
+    if (state == 0) {
+      return '{"lon":-0.13,"lat":51.51}';
+    } else if (state == 1) {
+      return '{"msg":"Auth error"}';
+    } else {
+      return '<html> <some>';
+    }
   }
 }
