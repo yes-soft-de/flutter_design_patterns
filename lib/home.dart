df0cdba1a5d9fbe127aafad5cc55b22dd9cc1691
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-08-05 11:18:30 +0300

    added Streams

--- lib/home.dart
+++ lib/home.dart
@@ -2,74 +2,94 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:patterns_app/coord_model.dart';
-import 'package:patterns_app/fake_server.dart';
+
+import 'home_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
+  final HomeBloc bloc = new HomeBloc();
+
   @override
   State<StatefulWidget> createState() => _HomeScreenState();
 }
+
 // Home Page State:
-/*
-      1. Displays UI
-      2. Request from Backend
-      3. Decode the response
-      4. Error Handling
-      5. State Management
- */
 class _HomeScreenState extends State<HomeScreen> {
-  FakeServer _server = FakeServer();
+  int currentState = HomeBloc.STATUS_CODE_INIT;
+  CoordModel payload;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: _server.getWeather(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Scaffold(
-            body: Center(child: Text('Error Loading Data')),
-          );
-        }
+    widget.bloc.stateStream.listen((event) {
+      currentState = event[HomeBloc.KEY_STATUS];
 
-        CoordModel response = CoordModel.fromJson(jsonDecode(snapshot.data));
+      if (currentState == HomeBloc.STATUS_CODE_SUCCESS) {
+        payload = event[HomeBloc.KEY_PAYLOAD];
+      }
 
-        return Scaffold(
-          body: Center(
-              child: Column(
+      setState(() {});
+    });
+
+    switch (currentState) {
+      case HomeBloc.STATUS_CODE_INIT:
+        widget.bloc.getWeather();
+        return drawLoadingScreen();
+      case HomeBloc.STATUS_CODE_LOADING:
+        return drawLoadingScreen();
+      case HomeBloc.STATUS_CODE_SUCCESS:
+        return drawSuccessScreen(
+            payload.lat.toString(), payload.lon.toString());
+      default:
+        return drawErrorScreen();
+    }
+  }
+
+  Scaffold drawSuccessScreen(String lat, String lon) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text(
+            'Weather Application',
+            style: TextStyle(color: Colors.blue, fontSize: 24),
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text(
-                'Weather Application',
-                style: TextStyle(color: Colors.blue, fontSize: 24),
+              Flex(
+                direction: Axis.horizontal,
+                children: <Widget>[Text('lon: '), Text(lon)],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Flex(
-                    direction: Axis.horizontal,
-                    children: <Widget>[
-                      Text('lon: '),
-                      Text(response.lon.toString())
-                    ],
-                  ),
-                  Flex(
-                    direction: Axis.horizontal,
-                    children: <Widget>[
-                      Text('lat: '),
-                      Text(response.lat.toString())
-                    ],
-                  )
-                ],
-              ),
-              RaisedButton(
-                onPressed: () {
-                  _server.getWeather();
-                },
-                child: Text('Refresh'),
+              Flex(
+                direction: Axis.horizontal,
+                children: <Widget>[Text('lat: '), Text(lat)],
               )
             ],
-          )),
-        );
-      },
+          ),
+          RaisedButton(
+            onPressed: () {
+              _server.getWeather();
+            },
+            child: Text('Refresh'),
+          )
+        ],
+      )),
+    );
+  }
+
+  Scaffold drawErrorScreen() {
+    return Scaffold(
+      body: Center(
+        child: Text('Error Loading Data'),
+      ),
+    );
+  }
+
+  Scaffold drawLoadingScreen() {
+    return Scaffold(
+      body: Center(
+        child: Text('Loading'),
+      ),
     );
   }
 }
--- lib/home_bloc.dart
+++ lib/home_bloc.dart
@@ -1,3 +1,46 @@
-class HomeBloc {
+import 'dart:convert';
 
+import 'package:patterns_app/coord_model.dart';
+import 'package:patterns_app/fake_server.dart';
+import 'package:rxdart/rxdart.dart';
+
+class HomeBloc {
+  static const int STATUS_CODE_INIT = 784;
+  static const int STATUS_CODE_SUCCESS = 785;
+  static const int STATUS_CODE_ERROR = 786;
+  static const int STATUS_CODE_LOADING = 787;
+
+  static const String KEY_STATUS = 'status';
+  static const String KEY_PAYLOAD = 'payload';
+
+  FakeServer _server;
+
+  HomeBloc() {
+    _server = new FakeServer();
+  }
+
+  PublishSubject<Map<String, dynamic>> _stateSubject = new PublishSubject();
+
+  Stream<Map<String, dynamic>> get stateStream => _stateSubject.stream;
+
+  getWeather() {
+    _stateSubject.add({KEY_STATUS: STATUS_CODE_LOADING});
+    _server.getWeather().then((value) {
+      try {
+        CoordModel model = CoordModel.fromJson(jsonDecode(value));
+        if (model.lon != null && model.lat != null) {
+          _stateSubject
+              .add({KEY_STATUS: STATUS_CODE_SUCCESS, KEY_PAYLOAD: model});
+        } else {
+          _stateSubject.add({KEY_STATUS: STATUS_CODE_ERROR});
+        }
+      } catch (e) {
+        _stateSubject.add({KEY_STATUS: STATUS_CODE_ERROR});
+      }
+    });
+  }
+
+  dispose() {
+    _stateSubject.close();
+  }
 }
